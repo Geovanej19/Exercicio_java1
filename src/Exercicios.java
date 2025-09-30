89ffb3b1a5bdad890d31b762f6cb7afe19aa48e2
Geovanej19 <Geovanej19>
2025-09-30 20:48:17 -0300

    Operadores Aritméticos

---
+++ src/Exercicios.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class jokenpo {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        Calculos calculos = new Calculos();
+
+        calculos.obterDados();
+    }
+}
